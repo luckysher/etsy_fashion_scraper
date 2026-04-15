627c39b104b52a1d744c5921b91fe75924f79b75
shersingh <luckyshera95@gmail.com>
2026-04-15 11:01:07 +0530

    added selenium import statement

--- etsy_fashion_scraper.py
+++ etsy_fashion_scraper.py
@@ -1,7 +1,11 @@
 import time
 import requests
 import os
-
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class EtsyFashionScraper:
 
