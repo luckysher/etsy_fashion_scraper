c1186f1187ed6bede9252b272e2b898ef30ab55b
shersingh <luckyshera95@gmail.com>
2026-04-15 10:25:46 +0530

    added etsy scraper file

---
+++ etsy_fashion_scraper.py
@@ -0,0 +1,23 @@
+import time
+import requests
+import os
+
+
+class EtsyFashionScraper:
+
+    def __init__(self):
+        self.base_url = "https://etsy.com"
+
+    def do_scraper_log(self, log_string=""):
+        """
+        Method for print formatted scraper log
+        :param log_string: String to log on terminal
+        :return:
+        """
+        pass
+
+    def go_to_clothes_category(self):
+        pass
+
+    def find_safe_value(self, content, tag, class_name=""):
+        pass
